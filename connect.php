cae1b3a6a0e8ca026f107fff0cda504b0ddda590
kwils205 <70655366+kwils205@users.noreply.github.com>
2020-09-30 18:14:29 -0400

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$host = kamweb3.database.windows.net;
+$username = 'kwils205';
+$password = 'Leidybug2020';
+$db_name = 'kamweb3';
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
+?>
